61c77816917abf5187e5e65ca05f8d6bae970850
野村健人 <nomurataketo@Kento.local>
2022-07-15 16:07:12 +0900

    [Add] 4章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY = 24;
+		String name = "のむ";
+
+		System.out.println(name);
+		name = "のむら";
+		System.out.println(name);
+		System.out.println(LUCKY);
+	}
+
+}
