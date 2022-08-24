6e43a603bb9e9e83693802f1c4a5faf825b74d31
Kentnomura <kentojaga0706@icloud.com>
2022-08-24 11:26:30 +0900

    [add] java

--- JavaWcp/src/person/Person.java
+++ JavaWcp/src/person/Person.java
@@ -1,33 +1,34 @@
 package person;
 
 class Person {
-	  public String firstName;
-	  public String lastName;
-	  public int age;
-	  public double height;
-	  public double weight;
+	  public static void printData(String name, int age, double height, double weight) {
+	    System.out.println("私の名前は" + name + "です");
+	    System.out.println("年齢は" + age + "歳です");
+	    System.out.println("身長は" + height + "mです");
+	    System.out.println("体重は" + weight + "kgです");
+	    double bmi = bmi(height, weight);
+	    System.out.println("BMIは" + Math.round(bmi) + "です");
 
-	  Person(String firstName, String lastName, int age, double height, double weight) {
-	    this.firstName = firstName;
-	    this.lastName = lastName;
-	    this.age = age;
-	    this.height = height;
-	    this.weight = weight;
+	    if (isHealthy(bmi)) {
+	      System.out.println("標準値です");
+	    } else {
+	      System.out.println("標準値の範囲外です");
+	    }
 	  }
 
-	  public String fullName() {
-	    return this.firstName + " " + this.lastName;
+	  public static String fullName(String firstName, String lastName) {
+	    return firstName + " " + lastName;
 	  }
 
-	  public double bmi() {
-	    return this.weight / this.height / this.height;
+	  public static String fullName(String firstName, String middleName, String lastName) {
+	    return firstName + " " + middleName + " " + lastName;
 	  }
 	  
-	  // printDataメソッドを定義してください
-	  public void printData() {
-	    System.out.println("私の名前は"+this.fullName()+"です");
-	    System.out.println("年齢は"+this.age+"歳です");
-	    System.out.println("BMIは"+Math.round(this.bmi())+"です");
+	  public static double bmi(double height, double weight) {
+	    return weight / height / height;
 	  }
 	  
+	  public static boolean isHealthy(double bmi) {
+	    return bmi >= 18.5 && bmi < 25.0;
+	  }
 	}
