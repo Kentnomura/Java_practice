833ac2e25ef6c03f7999288ccb980e8be9d5ec49
Kentnomura <kentojaga0706@icloud.com>
2022-08-25 11:12:16 +0900

    [Add] ja

--- JavaWcp/src/person/Main.java
+++ JavaWcp/src/person/Main.java
@@ -1,33 +1,17 @@
 package person;
 
-import java.util.Scanner;
-
 class Main {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    
-    System.out.print("名前：");
-    // 文字列の入力を受け取ってください
-    String firstName = scanner.next();
-    
-    System.out.print("名字：");
-    // 文字列の入力を受け取ってください
-    String lastName = scanner.next();
-    
-    System.out.print("年齢：");
-    // 整数の入力を受け取ってください
-    int age = scanner.nextInt();
-    
-    System.out.print("身長(m)：");
-    // 小数の入力を受け取ってください
-    double height = scanner.nextDouble();
-    
-    System.out.print("体重(kg)：");
-    // 小数の入力を受け取ってください
-    double weight = scanner.nextDouble();
-    
-    Person.printData(Person.fullName(firstName, lastName), age, height, weight);
-  }
-}
+	  public static void main(String[] args) {
+	    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+	    person1.printData();
+	    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
+	    person2.printData();
 
-
+	    System.out.println("----------------------");
+	    // person1のmiddleNameフィールドの値を「Claire」にしてください
+	    person1.setMiddleName("Claire");
+	    
+	    System.out.println("ミドルネームを" + person1.getMiddleName() + "に変更しました");
+	    person1.printData();
+	  }
+	}
--- JavaWcp/src/person/Person.java
+++ JavaWcp/src/person/Person.java
@@ -1,34 +1,56 @@
 package person;
 
 class Person {
-	  public static void printData(String name, int age, double height, double weight) {
-	    System.out.println("私の名前は" + name + "です");
-	    System.out.println("年齢は" + age + "歳です");
-	    System.out.println("身長は" + height + "mです");
-	    System.out.println("体重は" + weight + "kgです");
-	    double bmi = bmi(height, weight);
-	    System.out.println("BMIは" + Math.round(bmi) + "です");
+	  private static int count = 0;
+	  private String firstName;
+	  private String middleName;
+	  private String lastName;
+	  private int age;
+	  private double height;
+	  private double weight;
 
-	    if (isHealthy(bmi)) {
-	      System.out.println("標準値です");
+	  Person(String firstName, String lastName, int age, double height, double weight) {
+	    Person.count++;
+	    this.firstName = firstName;
+	    this.lastName = lastName;
+	    this.age = age;
+	    this.height = height;
+	    this.weight = weight;
+	  }
+
+	  Person(String firstName, String middleName, String lastName, int age, double height, double weight) {
+	    this(firstName, lastName, age, height, weight);
+	    this.middleName = middleName;
+	  }
+
+	  public String getMiddleName() {
+	    return this.middleName;
+	  }
+
+	  // middleNameフィールドのセッターを定義してください
+	  public void setMiddleName(String middleName) {
+	    this.middleName = middleName;
+	  }
+
+	  public String fullName() {
+	    if (this.middleName == null) {
+	      return this.firstName + " " + this.lastName;
 	    } else {
-	      System.out.println("標準値の範囲外です");
+	      return this.firstName + " " + this.middleName + " " + this.lastName;
 	    }
 	  }
 
-	  public static String fullName(String firstName, String lastName) {
-	    return firstName + " " + lastName;
+	  public void printData() {
+	    System.out.println("私の名前は" + this.fullName() + "です");
+	    System.out.println("年齢は" + this.age + "歳です");
+	    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
 	  }
 
-	  public static String fullName(String firstName, String middleName, String lastName) {
-	    return firstName + " " + middleName + " " + lastName;
+	  public double bmi() {
+	    return this.weight / this.height / this.height;
 	  }
-	  
-	  public static double bmi(double height, double weight) {
-	    return weight / height / height;
-	  }
-	  
-	  public static boolean isHealthy(double bmi) {
-	    return bmi >= 18.5 && bmi < 25.0;
+
+	  public static void printCount() {
+	    System.out.println("合計" + Person.count + "人です");
 	  }
 	}
